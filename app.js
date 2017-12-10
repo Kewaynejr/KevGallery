5a3372d5045bfdaa12ab905671274572b1f50260
Michael Templeton <michaelte@hightouchinc.com>
2017-12-10 14:22:27 -0600

    CRUD completely finished. Ready for styling

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(methodOverride("_method"));
 // 	body: "This is the body of my first post"
 // });
 
+// Get Homepage
 app.get("/", function(req, res) {
 	Post.find({}, function(err, foundPosts) {
         if (err) {
@@ -31,6 +32,7 @@ app.get("/", function(req, res) {
     });
 });
 
+// Get Gallery Page, not implemented right now
 app.get("/gallery", function(req, res) {
 	Post.find({}, function(err, foundPosts) {
 		if (err) {
@@ -41,6 +43,7 @@ app.get("/gallery", function(req, res) {
 	});
 });
 
+// Send new Posts to this route in order to save
 app.post("/gallery", function(req, res) {
 	req.body.post.body = req.sanitize(req.body.post.body);
 	Post.create(req.body.post, function(err, newPost) {
@@ -53,10 +56,12 @@ app.post("/gallery", function(req, res) {
 	});
 });
 
+// Show Form to create a new Post
 app.get("/gallery/new", function(req, res) {
 	res.render("gallery/new");
 });
 
+// Get one particular post by its ID
 app.get("/gallery/:id", function(req, res) {
 	Post.findById(req.params.id, function(err, foundPost) {
 		if (err) {
@@ -66,12 +71,44 @@ app.get("/gallery/:id", function(req, res) {
 			res.render("gallery/show", { post: foundPost });
 		}
 	});
-}); 
-
-app.get("/gallery/:id/edit", function(req, res) {
-	res.render("gallery/edit");
 });
 
+// Send updates to a particular post to this route
+app.put("/gallery/:id", function(req, res) {
+	Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost) {
+		if (err) {
+			console.log(err);
+			res.redirect("/gallery");
+		} else {
+			res.redirect("/gallery/" + req.params.id);
+		}
+	});
+});
+
+// Get Form that is used to update a particular post
+app.get("/gallery/:id/edit", function(req, res) {
+	Post.findById(req.params.id, function(err, foundPost) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("gallery/edit", { post: foundPost });
+        }
+	});
+});
+
+// Delete particular post Route
+app.delete("/gallery/:id", function(req, res) {
+	Post.findByIdAndRemove(req.params.id, function(err){
+		if (err) {
+			console.log(err);
+			res.redirect("/gallery");
+		} else {
+			res.redirect("/gallery");
+		}
+	});
+})
+
+// Server statement, adjust this to fit different environments
 app.listen(8000, process.env.IP, function() {
    console.log("KevGallery has started!");
 });
